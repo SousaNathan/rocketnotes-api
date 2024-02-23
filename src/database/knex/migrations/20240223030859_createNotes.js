f9749193ff2f4567b6e326f6cba8d1dd93bf3fdf
Nathan Sousa <nathan.wks@outlook.com>
2024-02-23 01:29:44 -0300

    knex, migretions orm

---
+++ src/database/knex/migrations/20240223030859_createNotes.js
@@ -0,0 +1,18 @@
+exports.up = knex => 
+  knex.schema
+  .createTable("notes", table => {
+    table.increments("id");
+    table.text("title");
+    table.text("description");
+    table.timestamp("created_at")
+      .default(knex.fn.now());
+    table.timestamp("updated_at")
+      .default(knex.fn.now());
+
+    table.integer("user_id")
+      .references("id").inTable("users");
+});
+
+exports.down = knex => 
+  knex.schema
+  .dropTable("notes");
